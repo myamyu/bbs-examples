d3f0d324574ae5aa90325957ef56008ad333fbac
みゃみゅ玉子 <myamyu@gmail.com>
2021-05-25 16:02:58 +0000

    ルーティング

--- backend/nim/src/service.nim
+++ backend/nim/src/service.nim
@@ -1,5 +1,39 @@
-import jester
+import jester, json
 
 routes:
   get "/":
     resp "こんにちはこんにちは"
+
+  # スレッド一覧
+  get "/threads":
+    resp %*{
+      "threadsCount": 0,
+      "count": 0,
+      "threads": [],
+    }
+
+  # スレッド新規作成
+  post "/threads":
+    let reqBody = parseJson(request.body())
+    echo reqBody
+    resp Http400, $(%*{
+      "error": [
+        {"field": "none", "message": "まだできてません"},
+      ],
+    }), "application/json"
+
+  # スレッド取得
+  get "/threads/@threadId":
+    let threadId = @"threadId"
+    echo threadId
+    resp Http404, $(%*{
+      "message": "見つからないよ",
+    }), "application/json"
+
+  # コメント投稿
+  post "/threads/@threadId/comments":
+    let threadId = @"threadId"
+    echo threadId
+    resp Http404, $(%*{
+      "message": "見つからないよ",
+    }), "application/json"
